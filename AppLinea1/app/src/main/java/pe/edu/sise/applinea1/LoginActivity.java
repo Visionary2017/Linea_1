b5fb32c0aab9ff274eba9351284bd2cae2b27e76
Visionary2017 <amamani.9508@gmail.com>
2017-08-28 22:51:22 -0500

    Actualizacion App1
    Add Activity de Splash, Login, Registro, Main

--- AppLinea1/app/src/main/java/pe/edu/sise/applinea1/LoginActivity.java
+++ AppLinea1/app/src/main/java/pe/edu/sise/applinea1/LoginActivity.java
@@ -1,13 +1,41 @@
 package pe.edu.sise.applinea1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    View toolbar;
+    TextView tvRegistro, tvVisitante;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        toolbar = (View) findViewById(R.id.toolbarLogin);
+        tvRegistro = (TextView) findViewById(R.id.tvRegistro);
+        tvVisitante = (TextView) findViewById(R.id.tvVisitante);
+        toolbar.setEnabled(false);
     }
+
+    public void fnOnClickRegistro(View view){
+
+        Intent intent = new Intent(getApplicationContext(),RegistroActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void fnOnClickVisitante(View view){
+
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(intent);
+
+    }
+
+
 }
---
+++ AppLinea1/app/src/main/java/pe/edu/sise/applinea1/splashActivity.java
@@ -0,0 +1,37 @@
+package pe.edu.sise.applinea1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class splashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(2000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(splashActivity.this,LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
