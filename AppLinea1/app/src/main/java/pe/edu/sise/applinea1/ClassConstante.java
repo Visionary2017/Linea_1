911e41352c2e7565139ec5817264deaac0bed3b7
Angel Jhonatan Mamani Puclla <amamani@rp3.com.ec>
2017-09-25 10:37:11 -0500

    Cambios Login Conexion Remota

---
+++ AppLinea1/app/src/main/java/pe/edu/sise/applinea1/ClassConstante.java
@@ -0,0 +1,16 @@
+package pe.edu.sise.applinea1;
+
+/**
+ * Created by Angel Mamani on 10/05/2017.
+ */
+
+public class ClassConstante {
+
+    //public static String DOMINIO = "http://192.168.1.45:8080/Archivos_Linea1/";
+    public static String DOMINIO = "https://linea1.000webhostapp.com/Linea1/";
+    public static String URL_IMG_MENU = "Imagenes/Estacion";
+    //public static String ENTIDAD_CATEGORIA = "Entidad_Persona.php";
+    //public static String ENTIDAD_PERSONA = "Entidad_Estacion.php";
+    public static String ACCESO_MENU = "Acceso.php";
+
+}
--- AppLinea1/app/src/main/java/pe/edu/sise/applinea1/LoginActivity.java
+++ AppLinea1/app/src/main/java/pe/edu/sise/applinea1/LoginActivity.java
@@ -3,18 +3,28 @@ package pe.edu.sise.applinea1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.DataOutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
+
+import cz.msebera.android.httpclient.Header;
+
+import static pe.edu.sise.applinea1.ClassConstante.*;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -61,8 +71,9 @@ public class LoginActivity extends AppCompatActivity {
 //                    }
 //                };
 //                tr.start();
-                Intent intent=new Intent(getApplicationContext(),MenuPrincipalActivity.class);
-                startActivity(intent);
+                /*Intent intent=new Intent(getApplicationContext(),MenuPrincipalActivity.class);
+                startActivity(intent);*/
+                Acceso_Menu();
             }
         });
     }
@@ -107,7 +118,70 @@ public class LoginActivity extends AppCompatActivity {
     }
 
 
+    public void Acceso_Menu(){
+        AsyncHttpClient client = new AsyncHttpClient();
+        try {
+            String URL_LOGIN = DOMINIO + ACCESO_MENU;
+            RequestParams parametros = new RequestParams();
+            parametros.put("usuario",etUsu.getText().toString());
+            parametros.put("password",etPass.getText().toString());
 
+            client.get(URL_LOGIN, parametros, new AsyncHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                    if (statusCode == 200 && obtieneDatosJSON(new String(responseBody)).toString().equals("ERROR-01")) {
+                        etPass.setText("");
+                        btnLog.setEnabled(true);
+                        Toast.makeText(getApplicationContext(), "Credenciales incorrectas.", Toast.LENGTH_SHORT).show();
+                    }else if(statusCode == 200 && obtieneDatosJSON(new String(responseBody)).toString().equals("ERROR-02")){
+                        etPass.setText("");
+                        btnLog.setEnabled(true);
+                        Toast.makeText(getApplicationContext(), "Debe Llenar los datos.", Toast.LENGTH_SHORT).show();
+                    }else if(statusCode == 200){
+                        /*session.createLoginSession("Android Hive", "anroidhive@gmail.com");*/
+                        etUsu.setText("");
+                        etPass.setText("");
+                        etUsu.requestFocus();
+                        btnLog.setEnabled(true);
+                        Intent intent = new Intent(getApplicationContext(),MenuPrincipalActivity.class);
+                        intent.putExtra("valor",obtieneDatosJSON(new String(responseBody)).toString());
+                        startActivity(intent);
+                    }
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                    Toast.makeText(getApplicationContext(), "onFail", Toast.LENGTH_SHORT).show();
+                    btnLog.setEnabled(true);
+                }
+            });
+
+        }catch (Exception e){
+            Toast.makeText(getApplicationContext(), "Error - " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    public String obtieneDatosJSON(String response){
+        String texto="";
+        try {
+            //JSONObject jsonObject2 = new JSONObject(response);
+            //texto = jsonArray2.getString("nombres_completo");
+            //JSONArray jsonArray2 = new JSONArray(response);
+            //texto = (jsonArray2.getJSONObject(0).getString("nombres_completo"));
+            JSONObject object = new JSONObject(response);
+            JSONArray Jarray  = object.getJSONArray("persona");
+
+            for (int i = 0; i < Jarray.length(); i++)
+            {
+                texto = Jarray.getJSONObject(i).getString("nombres_completo");
+            }
+            Log.i("texto-valor ",texto);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return texto;
+    }
 
    //////////////////
 
