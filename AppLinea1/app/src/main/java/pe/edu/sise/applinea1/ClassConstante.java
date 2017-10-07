a2081d597ff48d1ca18b1d119f3743d10d2368aa
LordEscanor <mysalinasgonzales@gmail.com>
2017-10-07 12:16:06 -0500

    G

--- AppLinea1/app/src/main/java/pe/edu/sise/applinea1/ClassConstante.java
+++ AppLinea1/app/src/main/java/pe/edu/sise/applinea1/ClassConstante.java
@@ -26,7 +26,7 @@ public class ClassConstante {
     //Para Listar las Estaciones
     public static String LISTAR_ESTACIONES= "ListEstacion.php";
 
-<<<<<<< HEAD
+
     //Para Restar saldo VISA
     public static String UDP_SALDO_VISA= "update_Saldo.php";
 
@@ -38,16 +38,8 @@ public class ClassConstante {
 
     //VERIFICAR ver saldo
     public static String MOSTRAR_SALDO= "ver_Saldo.php";
-=======
-
-    //Para Listar las Estaciones
-    public static String UDP_SALDO= "update_Saldo.php";
 
 
 
-    //Para Listar las Estaciones
-    public static String UDP_SALDO_VISA= "update_Saldo.php";
-
->>>>>>> 07fbe4e6c489d980dc8413af291bbfb4b4ca0098
 
 }
--- AppLinea1/app/src/main/java/pe/edu/sise/applinea1/PagoRealizadoActivity.java
+++ AppLinea1/app/src/main/java/pe/edu/sise/applinea1/PagoRealizadoActivity.java
@@ -3,18 +3,43 @@ package pe.edu.sise.applinea1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import cz.msebera.android.httpclient.Header;
+
+import static pe.edu.sise.applinea1.ClassConstante.ACCESO_MENU;
+import static pe.edu.sise.applinea1.ClassConstante.DOMINIO;
+import static pe.edu.sise.applinea1.ClassConstante.VER_SALDO;
 
 public class PagoRealizadoActivity extends AppCompatActivity {
 
     ImageButton imgButt;
-    TextView nue_sal,mont,sal_ant;
+    TextView nue_sal,mont,sal_ant, txt_fecha;
+    String numero_tarjeta;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pago_realizado);
+
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+        String formattedDate = df.format(c.getTime());
+
 
         imgButt = (ImageButton) findViewById(R.id.imageButton);
 
@@ -28,17 +53,69 @@ public class PagoRealizadoActivity extends AppCompatActivity {
 
         Bundle b = getIntent().getExtras();
        // String nuevo_saldo = b.getString("saldo_nuevo");
+        numero_tarjeta = b.getString("numero_tarjeta");
         double monto = b.getDouble("monto_recarga");
        // double saldo_anterior = Double.parseDouble(nuevo_saldo) - Double.parseDouble(monto);
 
         nue_sal = (TextView) findViewById(R.id.txtNuevoSaldo);
         mont = (TextView) findViewById(R.id.txtMontodeRecarga);
         sal_ant = (TextView) findViewById(R.id.txtSaldoAnterior);
+        txt_fecha = (TextView) findViewById(R.id.txtFechadeRecarga);
 
         //nue_sal.setText(""+nuevo_saldo);
-        mont.setText(""+monto);
+        mont.setText(numero_tarjeta+"/"+monto);
+        txt_fecha.setText(""+formattedDate);
         //sal_ant.setText(""+saldo_anterior);
 
+            Acceso_Menu();
 
+    }
+
+    public void Acceso_Menu(){
+        AsyncHttpClient client = new AsyncHttpClient();
+        try {
+            String URL_saldo = DOMINIO + VER_SALDO;
+            RequestParams parametros = new RequestParams();
+            parametros.put("nro_tarjeta",numero_tarjeta);
+
+            client.get(URL_saldo, parametros, new AsyncHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                    if (statusCode == 200 && obtieneDatosJSON(new String(responseBody)).toString().equals("ERROR-01")) {
+
+                    }else if(statusCode == 200){
+                        Toast.makeText(PagoRealizadoActivity.this, obtieneDatosJSON(new String(responseBody)).toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                    Toast.makeText(getApplicationContext(), "onFail", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+
+        }catch (Exception e){
+            Toast.makeText(getApplicationContext(), "Error - " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    public String obtieneDatosJSON(String response){
+        String texto="";
+        try {
+
+            JSONObject object = new JSONObject(response);
+            JSONArray Jarray  = object.getJSONArray("tarjeta");
+
+            for (int i = 0; i < Jarray.length(); i++)
+            {
+                texto = Jarray.getJSONObject(i).getString("nro_Tarjeta");
+            }
+            Log.i("texto-valor ",texto);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return texto;
     }
 }
