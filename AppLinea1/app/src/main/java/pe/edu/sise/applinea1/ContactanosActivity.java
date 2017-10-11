75e8f9d46f0fed01c1d1715658cc0e9b2720a3c6
jcastillo20 <juancrcastillo20@gmail.com>
2017-10-11 15:33:53 -0500

    contacto

--- AppLinea1/app/src/main/java/pe/edu/sise/applinea1/ContactanosActivity.java
+++ AppLinea1/app/src/main/java/pe/edu/sise/applinea1/ContactanosActivity.java
@@ -1,5 +1,6 @@
 package pe.edu.sise.applinea1;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -13,23 +14,40 @@ import com.squareup.picasso.Picasso;
 
 public class ContactanosActivity extends AppCompatActivity {
 
-    ImageButton imgFB;
+    ImageButton imgFB,imgYou;
     private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contactanos);
         imgFB=(ImageButton)findViewById(R.id.imgFacebook);
+        imgYou=(ImageButton)findViewById(R.id.imgYoutube);
 
 
         imgFB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String facebookId = "fb://page/<206969889390420>";
-                String urlPage = "http://www.facebook.com/Lineauno.pe";
+                String facebookId = "fb://page/1488069544613775";
+                String urlPage = "https://www.facebook.com/Lineauno.pe";
 
                 try {
                     startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(facebookId )));
+                } catch (Exception e) {
+
+                    //Abre url de pagina.
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(urlPage)));
+                }
+            }
+        });
+
+        imgYou.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String youtubeID = "com.youtube";
+                String urlPage = "https://www.youtube.com/user/Lineaunope";
+
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(youtubeID )));
                 } catch (Exception e) {
 
                     //Abre url de pagina.
