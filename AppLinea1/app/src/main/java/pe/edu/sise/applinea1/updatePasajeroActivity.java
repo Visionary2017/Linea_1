1ce8194da382b7f31c384a079db4b702ecc82542
jcastillo20 <juancrcastillo20@gmail.com>
2017-10-13 08:05:29 -0500

    ultimo

--- AppLinea1/app/src/main/java/pe/edu/sise/applinea1/updatePasajeroActivity.java
+++ AppLinea1/app/src/main/java/pe/edu/sise/applinea1/updatePasajeroActivity.java
@@ -4,13 +4,11 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-<<<<<<< HEAD
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-=======
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -18,7 +16,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
->>>>>>> 2041bfbbfddd0973f68c6f03c1504b70d520a5bb
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -49,28 +46,19 @@ public class updatePasajeroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_pasajero);
-<<<<<<< HEAD
-=======
         navigationView=(NavigationView)findViewById(R.id.navview);
         drawerLayout=(DrawerLayout) findViewById(R.id.drawer_ActualizarContacto);
->>>>>>> 2041bfbbfddd0973f68c6f03c1504b70d520a5bb
         nombre = (EditText) findViewById(R.id.etNombreCompletoUpdate);
         apellido = (EditText) findViewById(R.id.etApellidoCompletoUpdate);
         celular = (EditText) findViewById(R.id.etTelefonoUpdate);
         correo = (EditText) findViewById(R.id.etCorreoUpdate);
         btnActualizar = (Button) findViewById(R.id.btnUpdate);
-<<<<<<< HEAD
-
-=======
         setToolbar();
->>>>>>> 2041bfbbfddd0973f68c6f03c1504b70d520a5bb
         datos = this.getIntent().getExtras();
        numero_tarjeta = datos.getString("numero_tarjeta");
 
         Toast.makeText(this, numero_tarjeta, Toast.LENGTH_SHORT).show();
 
-<<<<<<< HEAD
-=======
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -115,7 +103,6 @@ public class updatePasajeroActivity extends AppCompatActivity {
                 return true;
             }
         });
->>>>>>> 2041bfbbfddd0973f68c6f03c1504b70d520a5bb
         btnActualizar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
