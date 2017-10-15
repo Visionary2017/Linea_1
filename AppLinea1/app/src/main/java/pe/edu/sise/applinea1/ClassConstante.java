21fb5d21851748747ca5999cd5cd97a9e2ccf3b7
Angel Jhonatan Mamani Puclla <amamani@rp3.com.ec>
2017-10-15 01:56:04 -0500

    Link de Dominio Actualizado

--- AppLinea1/app/src/main/java/pe/edu/sise/applinea1/ClassConstante.java
+++ AppLinea1/app/src/main/java/pe/edu/sise/applinea1/ClassConstante.java
@@ -1,8 +1,8 @@
 package pe.edu.sise.applinea1;
 
 public class ClassConstante {
-    public static String DOMINIO = "http://192.168.1.36:8080/Archivos_Linea1/";
-    //public static String DOMINIO = "https://linea1.000webhostapp.com/Linea1/";
+    //public static String DOMINIO = "http://192.168.1.36:8080/Archivos_Linea1/";
+    public static String DOMINIO = "https://linea1.000webhostapp.com/Linea1/";
     public static String URL_IMG = "Imagenes/Estaciones";
     //public static String ENTIDAD_CATEGORIA = "Entidad_Persona.php";
     //public static String ENTIDAD_PERSONA = "Entidad_Estacion.php";
